af2d0e15e7de8a2b51bad2dcc109cb8ee2e74d8f
rhama-krisner <rhamakrisner@gmail.com>
2021-10-23 17:51:34 -0300

    Update admin.py

--- empresa/admin.py
+++ empresa/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Departamento,Projeto, Funcionarios
 
 # Register your models here.
+
+class ProjetoAdmin(admin.ModelAdmin):
+    list_display = ('id','nome')
+
+class DepartamentoAdmin(admin.ModelAdmin):
+    list_display = ('id','nome')
+
+class FuncionariosAdmin(admin.ModelAdmin):
+    list_display = ('id','nome')
+
+admin.site.register(Projeto, ProjetoAdmin)
+admin.site.register(Departamento, DepartamentoAdmin)
+admin.site.register(Funcionarios, FuncionariosAdmin)
