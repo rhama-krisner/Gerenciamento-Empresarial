56dd029eedd2d1e52af87eed2945afbad39d3ea0
rhama-krisner <rhamakrisner@gmail.com>
2021-10-23 23:15:15 -0300

    Update views.py

--- empresa/views.py
+++ empresa/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+
+@api_view(['GET'])
+def empresa(request):
+    return Response({'status':200, 'message':'Olá mundo' })
