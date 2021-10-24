2c2ffb37b361224592d6ac07c645a4169552c3a5
rhama-krisner <rhamakrisner@gmail.com>
2021-10-24 15:51:31 -0300

    Update urls.py

--- setup/urls.py
+++ setup/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+from empresa import views
+
+router = routers.DefaultRouter()
+
+router.register(r'projeto', views.Projeto)
+router.register(r'funcionarios', views.Funcionarios)
 
 urlpatterns = [
-    path('', include('empresa.urls')),
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
