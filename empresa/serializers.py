affe4096a2b3acdafa6203c3f8e07299c45ff150
rhama-krisner <rhamakrisner@gmail.com>
2021-10-26 16:56:16 -0300

    Update serializers.py

--- empresa/serializers.py
+++ empresa/serializers.py
@@ -1,28 +1,28 @@
 from rest_framework import serializers
 from .models import Departamento, Projeto, Funcionarios
 
-class ProjetoSerializer(serializers.ModelSerializer):
+class FuncionariosSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Funcionarios
+        fields = '__all__'
 
+class ProjetoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Projeto
         fields = ['id', 'nome', 'horas', 
         'prazo', 'horas_realizadas', 
         'ultimo_calculo_de_horas','departamento']
 
-class DepartamentoSerializer(serializers.ModelSerializer):
-    projeto = serializers.RelatedField(many=True, read_only=True)
 
+class DepartamentoSerializer(serializers.ModelSerializer):
+    projeto = serializers.SlugRelatedField(many=True, read_only=True, slug_field='nome')
     class Meta:
         model = Departamento
         fields = ['id', 'nome','projeto']
         
 
 
-class FuncionariosSerializer(serializers.ModelSerializer):   
 
-    class Meta:
-        model = Funcionarios
-        fields = '__all__'
 
 
 
