627fb5dac853ac40a3b3009c1dbe766e8016ec29
rhama-krisner <rhamakrisner@gmail.com>
2021-10-27 14:37:43 -0300

    Update

--- empresa/serializers.py
+++ empresa/serializers.py
@@ -4,23 +4,22 @@ from .models import Departamento, Projeto, Funcionarios
 class FuncionariosSerializer(serializers.ModelSerializer):
     class Meta:
         model = Funcionarios
-        fields = ('id', 'nome','cpf','rg','sexo','data_nascimento','cnh','salario','carga_horaria')
+        fields = ['id', 'nome','cpf','rg','sexo','data_nascimento','cnh','salario','carga_horaria']
 
 
 class ProjetoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Projeto
-        fields = ('id', 'nome', 'horas', 
+        fields = ['id', 'nome', 'horas', 
         'prazo', 'horas_realizadas', 
-        'ultimo_calculo_de_horas')
+        'ultimo_calculo_de_horas']
 
 
 class DepartamentoSerializer(serializers.ModelSerializer):
-    projeto = serializers.RelatedField(many=True, read_only=True)
-
+    projeto = serializers.StringRelatedField(many=True, read_only=True)
     class Meta:
         model = Departamento
-        fields = ('id', 'nome')
+        fields = ['id', 'nome','projeto']
         
 
 
